a9773f71ca5b6e32a62d0e5926c53397079450e6
tareq <tarekian99@gmail.com>
2024-06-12 12:13:24 +0300

    refactor

--- routes/channels.php
+++ routes/channels.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Broadcast;
 use App\Models\User;
 
-Broadcast::channel('chat.{receiver_id}', fn (User $user, $receiver_id) => true);
+Broadcast::channel('chat.{receiver_id}', fn (User $user, $receiver_id) => (int)$user->id === (int)$receiver_id);
 Broadcast::channel('online-status', fn ($user) => $user);
